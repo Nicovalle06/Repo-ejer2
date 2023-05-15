180569fe2ea4e5827bf0a56c42391a2f06d671aa
Emiliano A. Gerzel <emilianogerzel@gmail.com>
2023-05-15 19:36:45 -0300

    Modifique el link de la linea 14

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="Este link se rompio :D"
           target="_blank"
           rel="noopener noreferrer"
         >
